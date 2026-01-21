436662c6eb416b47d6412ed907b1a18b25f6bd58
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-01-21 15:20:15 +0530

    app.js added successfuly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [data, setData] = useState({ skills: [] });
+
+  return <Dashboard data={data} setData={setData} />;
 }
-
-export default App;
